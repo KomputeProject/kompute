443679f7d38731519799cfe09406784239ceb34a
Alejandro Saucedo <axsauze@gmail.com>
2020-07-31 06:46:01 +0100

    Updated to mozilla format

--- src/VulkanTools.h
+++ src/VulkanTools.h
@@ -13,13 +13,13 @@
 #include "vulkan/vulkan.h"
 
 #include <assert.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <math.h>
 #include <stdexcept>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string>
 #include <vector>
 #if defined(_WIN32)
@@ -34,18 +34,19 @@
 #define DEFAULT_FENCE_TIMEOUT 100000000000
 
 // Macro to check and display Vulkan return results
-#define VK_CHECK_RESULT(f)                                                    \
-    {                                                                         \
-        VkResult res = (f);                                                   \
-        if (res != VK_SUCCESS) {                                              \
-            std::cout << "Fatal : VkResult is \""                             \
-                      << vks::tools::errorString(res) << "\" in " << __FILE__ \
-                      << " at line " << __LINE__ << std::endl;                \
-            assert(res == VK_SUCCESS);                                        \
-        }                                                                     \
+#define VK_CHECK_RESULT(f)                                                     \
+    {                                                                          \
+        VkResult res = (f);                                                    \
+        if (res != VK_SUCCESS) {                                               \
+            std::cout << "Fatal : VkResult is \""                              \
+                      << vks::tools::errorString(res) << "\" in " << __FILE__  \
+                      << " at line " << __LINE__ << std::endl;                 \
+            assert(res == VK_SUCCESS);                                         \
+        }                                                                      \
     }
 
-const std::string getAssetPath();
+const std::string
+getAssetPath();
 
 namespace vks {
 namespace tools {
@@ -53,61 +54,71 @@ namespace tools {
 extern bool errorModeSilent;
 
 /** @brief Returns an error code as a string */
-std::string errorString(VkResult errorCode);
+std::string
+errorString(VkResult errorCode);
 
 /** @brief Returns the device type as a string */
-std::string physicalDeviceTypeString(VkPhysicalDeviceType type);
+std::string
+physicalDeviceTypeString(VkPhysicalDeviceType type);
 
 // Selected a suitable supported depth format starting with 32 bit down to 16
 // bit Returns false if none of the depth formats in the list is supported by
 // the device
-VkBool32 getSupportedDepthFormat(VkPhysicalDevice physicalDevice,
-                                 VkFormat* depthFormat);
+VkBool32
+getSupportedDepthFormat(VkPhysicalDevice physicalDevice, VkFormat* depthFormat);
 
 // Returns if a given format support LINEAR filtering
-VkBool32 formatIsFilterable(VkPhysicalDevice physicalDevice,
-                            VkFormat format,
-                            VkImageTiling tiling);
+VkBool32
+formatIsFilterable(VkPhysicalDevice physicalDevice,
+                   VkFormat format,
+                   VkImageTiling tiling);
 
 // Put an image memory barrier for setting an image layout on the sub resource
 // into the given command buffer
-void setImageLayout(
-    VkCommandBuffer cmdbuffer,
-    VkImage image,
-    VkImageLayout oldImageLayout,
-    VkImageLayout newImageLayout,
-    VkImageSubresourceRange subresourceRange,
-    VkPipelineStageFlags srcStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
-    VkPipelineStageFlags dstStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
+void
+setImageLayout(
+        VkCommandBuffer cmdbuffer,
+        VkImage image,
+        VkImageLayout oldImageLayout,
+        VkImageLayout newImageLayout,
+        VkImageSubresourceRange subresourceRange,
+        VkPipelineStageFlags srcStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
+        VkPipelineStageFlags dstStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
 // Uses a fixed sub resource layout with first mip level and layer
-void setImageLayout(
-    VkCommandBuffer cmdbuffer,
-    VkImage image,
-    VkImageAspectFlags aspectMask,
-    VkImageLayout oldImageLayout,
-    VkImageLayout newImageLayout,
-    VkPipelineStageFlags srcStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
-    VkPipelineStageFlags dstStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
+void
+setImageLayout(
+        VkCommandBuffer cmdbuffer,
+        VkImage image,
+        VkImageAspectFlags aspectMask,
+        VkImageLayout oldImageLayout,
+        VkImageLayout newImageLayout,
+        VkPipelineStageFlags srcStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
+        VkPipelineStageFlags dstStageMask = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT);
 
 /** @brief Inser an image memory barrier into the command buffer */
-void insertImageMemoryBarrier(VkCommandBuffer cmdbuffer,
-                              VkImage image,
-                              VkAccessFlags srcAccessMask,
-                              VkAccessFlags dstAccessMask,
-                              VkImageLayout oldImageLayout,
-                              VkImageLayout newImageLayout,
-                              VkPipelineStageFlags srcStageMask,
-                              VkPipelineStageFlags dstStageMask,
-                              VkImageSubresourceRange subresourceRange);
+void
+insertImageMemoryBarrier(VkCommandBuffer cmdbuffer,
+                         VkImage image,
+                         VkAccessFlags srcAccessMask,
+                         VkAccessFlags dstAccessMask,
+                         VkImageLayout oldImageLayout,
+                         VkImageLayout newImageLayout,
+                         VkPipelineStageFlags srcStageMask,
+                         VkPipelineStageFlags dstStageMask,
+                         VkImageSubresourceRange subresourceRange);
 
 // Display error message and exit on fatal error
-void exitFatal(std::string message, int32_t exitCode);
-void exitFatal(std::string message, VkResult resultCode);
+void
+exitFatal(std::string message, int32_t exitCode);
+void
+exitFatal(std::string message, VkResult resultCode);
 
 // Load a SPIR-V shader (binary)
-VkShaderModule loadShader(const char* fileName, VkDevice device);
+VkShaderModule
+loadShader(const char* fileName, VkDevice device);
 
 /** @brief Checks if a file exists */
-bool fileExists(const std::string& filename);
-}  // namespace tools
-}  // namespace vks
+bool
+fileExists(const std::string& filename);
+} // namespace tools
+} // namespace vks
