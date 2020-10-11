dc9fb9fdec920bded71786e7b712c6fbee07275f
Alejandro Saucedo <axsauze@gmail.com>
2020-10-11 11:56:17 +0100

    publish: Updated cname to use kompute.cc url
    generated from commit d14056db211fac29dc12272d4ec7eb44335f672e

--- doxygen/search/all_4.js
+++ doxygen/search/all_4.js
@@ -5,6 +5,6 @@ var searchData=
   ['estaging_7',['eStaging',['../classkp_1_1Tensor.html#a1b2af1c0e8dc0f154e11780e7103beb4ab288dd012e30ff97c0236b12e3c2c6e5',1,'kp::Tensor']]],
   ['estorage_8',['eStorage',['../classkp_1_1Tensor.html#a1b2af1c0e8dc0f154e11780e7103beb4ae1a7e733531f08074becd467959f0521',1,'kp::Tensor']]],
   ['eval_9',['eval',['../classkp_1_1Sequence.html#aa0b36564769ba25a43a38e14a2d510dc',1,'kp::Sequence']]],
-  ['evalop_10',['evalOp',['../classkp_1_1Manager.html#af5ef1e29c5c4a2af3184016dc718f022',1,'kp::Manager']]],
+  ['evalop_10',['evalOp',['../classkp_1_1Manager.html#aeb89ce2a56d09d056561f021132a5469',1,'kp::Manager']]],
   ['evalopdefault_11',['evalOpDefault',['../classkp_1_1Manager.html#ae7a3f54442823198af724c97c87c3dc6',1,'kp::Manager']]]
 ];
--- doxygen/search/functions_4.js
+++ doxygen/search/functions_4.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['end_85',['end',['../classkp_1_1Sequence.html#ac51befb176acd2064d3e85133bf66ada',1,'kp::Sequence']]],
   ['eval_86',['eval',['../classkp_1_1Sequence.html#aa0b36564769ba25a43a38e14a2d510dc',1,'kp::Sequence']]],
-  ['evalop_87',['evalOp',['../classkp_1_1Manager.html#af5ef1e29c5c4a2af3184016dc718f022',1,'kp::Manager']]],
+  ['evalop_87',['evalOp',['../classkp_1_1Manager.html#aeb89ce2a56d09d056561f021132a5469',1,'kp::Manager']]],
   ['evalopdefault_88',['evalOpDefault',['../classkp_1_1Manager.html#ae7a3f54442823198af724c97c87c3dc6',1,'kp::Manager']]]
 ];
