743a6877042408cdab87ce75710385142bfe04ee
Alejandro Saucedo <axsauze@gmail.com>
2020-10-12 08:12:21 +0100

    removed unused imports in android jni file

--- examples/android/android-simple/app/src/main/cpp/KomputeJniNative.cpp
+++ examples/android/android-simple/app/src/main/cpp/KomputeJniNative.cpp
@@ -12,13 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#undef DEBUG
-#define RELEASE 1
-
-#include <android/log.h>
 #include <unistd.h>
 
-#include <string.h>
 #include <jni.h>
 
 #include "kompute/Kompute.hpp"
