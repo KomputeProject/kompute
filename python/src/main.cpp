6afe6463c2f737fad2aab39c01e4f5a3732e29b3
Alejandro Saucedo <axsauze@gmail.com>
2020-11-01 16:27:59 +0000

    Updated to add opbase

--- python/src/main.cpp
+++ python/src/main.cpp
@@ -21,7 +21,10 @@ PYBIND11_MODULE(komputepy, m) {
         .def(py::init(
             [](const std::vector<float>& data, kp::Tensor::TensorTypes tensorTypes) {
                 return std::unique_ptr<kp::Tensor>(new kp::Tensor(data, tensorTypes));
-            }));
+            }))
+        .def("data", &kp::Tensor::data);
+
+    py::class_<kp::OpBase>(m, "OpBase");
 
 #ifdef VERSION_INFO
     m.attr("__version__") = VERSION_INFO;
