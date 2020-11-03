0d33dc018d812cc1349e5140454eeec300b6dce0
Alejandro Saucedo <axsauze@gmail.com>
2020-11-03 08:04:37 +0000

    Updated version to 0.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2020, The Institute for Ethical AI & Machine Learning'
 author = 'Alejandro Saucedo'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.0'
+release = '0.4.1'
 
 
 # -- General configuration ---------------------------------------------------
