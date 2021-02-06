c8283844d6606691abda9e64415760a7fb0ee461
Alejandro Saucedo <axsauze@gmail.com>
2021-02-06 19:09:48 +0000

    Added destructor for staging tensors

--- src/OpTensorCreate.cpp
+++ src/OpTensorCreate.cpp
@@ -23,6 +23,16 @@ OpTensorCreate::OpTensorCreate(
 OpTensorCreate::~OpTensorCreate()
 {
     SPDLOG_DEBUG("Kompute OpTensorCreate destructor started");
+
+    SPDLOG_DEBUG("Kompute OpBase freeing staging tensors");
+    for (std::shared_ptr<Tensor> tensor : this->mStagingTensors) {
+        if (tensor && tensor->isInit()) {
+            tensor->freeMemoryDestroyGPUResources();
+        } else {
+            SPDLOG_ERROR("Kompute OpBase expected to free "
+                          "tensor but has already been freed.");
+        }
+    }
 }
 
 void
