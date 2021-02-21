34003587439929aa69f7674dccfae6898653aae5
Alejandro Saucedo <axsauze@gmail.com>
2021-02-21 12:51:34 +0000

    publish: Merge pull request #159 from EthicalML/158_fmt
    generated from commit 149e18a530f94c3133c7f23d126f18537a041003

--- doxygen/search/functions_2.js
+++ doxygen/search/functions_2.js
@@ -1,4 +1,6 @@
 var searchData=
 [
+  ['compile_5fsource',['compile_source',['../classkp_1_1Shader.html#a272acc94fcaed0a4c82a818c28b18fe8',1,'kp::Shader']]],
+  ['compile_5fsources',['compile_sources',['../classkp_1_1Shader.html#ae0d70e63b68f97b9c7fc6ffeb6ddfa14',1,'kp::Shader']]],
   ['constructdescriptorbufferinfo',['constructDescriptorBufferInfo',['../classkp_1_1Tensor.html#a805e6050a935763488116e071c0ce461',1,'kp::Tensor']]]
 ];
