0b1c4daebf78e0fc45d76b836e7013883ce51356
Alejandro Saucedo <axsauze@gmail.com>
2021-03-12 07:38:50 +0000

    publish: Fixed references in the docs
    generated from commit 8ec1f8000be1d22f00192c1f3d04e13c0c0cd15b

--- doxygen/search/all_5.js
+++ doxygen/search/all_5.js
@@ -3,5 +3,6 @@ var searchData=
   ['getpush',['getPush',['../classkp_1_1Algorithm.html#ac3b5b33da0d3d956b235c5fbfe12e3bc',1,'kp::Algorithm']]],
   ['getspecializationconstants',['getSpecializationConstants',['../classkp_1_1Algorithm.html#ad854fce0359f1d880adef5e523ac8ba0',1,'kp::Algorithm']]],
   ['gettensors',['getTensors',['../classkp_1_1Algorithm.html#a899e4fa43bf02bc556920fc3b4b22900',1,'kp::Algorithm']]],
+  ['gettimestamps',['getTimestamps',['../classkp_1_1Sequence.html#a4b4e0b9a04695b9b936aa1e51468cccb',1,'kp::Sequence']]],
   ['getworkgroup',['getWorkgroup',['../classkp_1_1Algorithm.html#a81ce1c093ff2e837f998e9962b2c09e3',1,'kp::Algorithm']]]
 ];
--- doxygen/search/all_b.js
+++ doxygen/search/all_b.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['sequence',['Sequence',['../classkp_1_1Sequence.html',1,'kp::Sequence'],['../classkp_1_1Sequence.html#a83518e6c7f249680a9a0a9e7d5270b9a',1,'kp::Sequence::Sequence()'],['../classkp_1_1Manager.html#a9d0c4070458e5949bfa18a589fe75e41',1,'kp::Manager::sequence()']]],
+  ['sequence',['Sequence',['../classkp_1_1Sequence.html',1,'kp::Sequence'],['../classkp_1_1Manager.html#a5fac648ac65d2e1314120ebf6f91de91',1,'kp::Manager::sequence()'],['../classkp_1_1Sequence.html#afb1974c612652d9c58b6e0bcfb83db4a',1,'kp::Sequence::Sequence()']]],
   ['setpush',['setPush',['../classkp_1_1Algorithm.html#a2a082021e3343bce5bed1f842885c67c',1,'kp::Algorithm']]],
   ['setrawdata',['setRawData',['../classkp_1_1Tensor.html#a70b8a0e812aa2746726052c4bfa7307e',1,'kp::Tensor']]],
   ['setworkgroup',['setWorkgroup',['../classkp_1_1Algorithm.html#a3f195f84f2e30d5913776a6610c52c19',1,'kp::Algorithm']]],
