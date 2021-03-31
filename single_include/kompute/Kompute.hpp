61053d633d8e5f1d84bb31a76d33e1291a498824
aliPMPAINT <ali.PMPAINT@tutanota.com>
2021-03-31 11:23:16 +0430

    Updating Kompute.hpp

--- single_include/kompute/Kompute.hpp
+++ single_include/kompute/Kompute.hpp
@@ -729,7 +729,7 @@ extern py::object kp_debug, kp_info, kp_warning, kp_error;
 #include <iostream>
 #include <vector>
 
-#if USE_EXTERNAL_SHADER
+#ifdef USE_EXTERNAL_GLSLANG
 #include <SPIRV/GlslangToSpv.h>
 #else 
 #include <glslang/SPIRV/GlslangToSpv.h>
