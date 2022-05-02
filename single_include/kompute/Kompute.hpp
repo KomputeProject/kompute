f40ba2852b082d0a7e3b5069221141a511483011
Fabian Sauter <sauter.fabian@mailbox.org>
2022-05-02 14:33:03 +0200

    Enum class fix for fmt 8.x
    Since fmt 8.0.0 enum classes are not being implicitly converted to int any more.
    Refference: https://github.com/fmtlib/fmt/issues/1841
    
    Signed-off-by: Fabian Sauter <sauter.fabian@mailbox.org>

--- single_include/kompute/Kompute.hpp
+++ single_include/kompute/Kompute.hpp
@@ -598,6 +598,8 @@ static const char* KOMPUTE_LOG_TAG = "KomputeLog";
 #endif
 
 #include <fmt/core.h>
+#include <fmt/format.h>
+#include <string>
 
 #include <vulkan/vulkan.hpp>
 
@@ -2368,3 +2370,37 @@ class Manager
 };
 
 } // End namespace kp
+
+/**
+ * fmt fromater for kp::Tensor::TensorDataTypes.
+ */
+template <> struct fmt::formatter<kp::Tensor::TensorDataTypes>: formatter<std::string> {
+  template <typename FormatContext>
+  auto format(kp::Tensor::TensorDataTypes dt, FormatContext& ctx) {
+    std::string name = "unknown";
+    switch (dt) {
+      case kp::Tensor::TensorDataTypes::eBool: name = "eBool"; break;
+      case kp::Tensor::TensorDataTypes::eDouble: name = "eDouble"; break;
+      case kp::Tensor::TensorDataTypes::eFloat: name = "eFloat"; break;
+      case kp::Tensor::TensorDataTypes::eInt: name = "eInt"; break;
+      case kp::Tensor::TensorDataTypes::eUnsignedInt: name = "eUnsignedInt"; break;
+    }
+    return formatter<std::string>::format(name, ctx);
+  }
+};
+
+/**
+ * fmt fromater for kp::Tensor::TensorTypes.
+ */
+template <> struct fmt::formatter<kp::Tensor::TensorTypes>: formatter<std::string> {
+  template <typename FormatContext>
+  auto format(kp::Tensor::TensorTypes dt, FormatContext& ctx) {
+    std::string name = "unknown";
+    switch (dt) {
+      case kp::Tensor::TensorTypes::eDevice: name = "eDevice"; break;
+      case kp::Tensor::TensorTypes::eHost: name = "eHost"; break;
+      case kp::Tensor::TensorTypes::eStorage: name = "eStorage"; break;
+    }
+    return formatter<std::string>::format(name, ctx);
+  }
+};
