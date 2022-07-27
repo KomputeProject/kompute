83d9a186289ac299cc7d55dcb63865a28b144240
Fabian Sauter <sauter.fabian@mailbox.org>
2022-07-27 12:41:10 +0200

    Removed .data() for deviceName
    Signed-off-by: Fabian Sauter <sauter.fabian@mailbox.org>
    Signed-off-by: jswag180 <jefferycole100@gmail.com>

--- python/src/utils.hpp
+++ python/src/utils.hpp
@@ -12,7 +12,7 @@ vkPropertiesToDict(const vk::PhysicalDeviceProperties& properties)
 {
 
     pybind11::dict pyDict(
-      "device_name"_a = std::string(properties.deviceName.data()),
+      "device_name"_a = std::string(properties.deviceName),
       "max_work_group_count"_a =
         pybind11::make_tuple(properties.limits.maxComputeWorkGroupCount[0],
                              properties.limits.maxComputeWorkGroupCount[1],
