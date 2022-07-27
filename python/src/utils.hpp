53a3753caf997cee19a4c8021fafab0ae71de5d2
Fabian Sauter <sauter.fabian@mailbox.org>
2022-07-27 12:59:08 +0200

    Fixed deviceName conversion
    Signed-off-by: Fabian Sauter <sauter.fabian@mailbox.org>

--- python/src/utils.hpp
+++ python/src/utils.hpp
@@ -10,9 +10,9 @@ namespace py {
 static pybind11::dict
 vkPropertiesToDict(const vk::PhysicalDeviceProperties& properties)
 {
-
+    std::string deviceName = properties.deviceName;
     pybind11::dict pyDict(
-      "device_name"_a = std::string(properties.deviceName),
+      "device_name"_a = deviceName,
       "max_work_group_count"_a =
         pybind11::make_tuple(properties.limits.maxComputeWorkGroupCount[0],
                              properties.limits.maxComputeWorkGroupCount[1],
